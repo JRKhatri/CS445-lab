82b875936964ac6f456731e70a7124f3498e4fac
JRKhatri <79480451+JRKhatri@users.noreply.github.com>
2021-05-22 01:03:01 -0500

    final commit all exercise

---
+++ lab3/src/exe3.ts
@@ -0,0 +1,42 @@
+/*Exercise 03
+Re-write the following code using TypeScript Class syntax. Try to be as explicit as possible and 
+add Types to everything you can. When you are done, transpile the TS code to JS code and inspect the JS code.
+
+function Car(name) { 
+	this.name = name; 
+	this.acceleration = 0; 
+	this.honk = function() { 
+		console.log(` ${this.name} is saying: Toooooooooot!`); 
+	}; 
+	this.accelerate = function(speed) { 
+		this.acceleration = this.acceleration + speed; 
+	} 
+} 
+var car = new Car("BMW"); 
+car.honk(); // BMW is saying: Toooooooooot!
+console.log(car.acceleration); // 0
+car.accelerate(60); 
+console.log(car.acceleration); // 60
+*/
+//solution
+
+class Car {
+    name : string;
+    acceleration : number = 0;
+    constructor (name : string){
+        this.name = name;
+    }
+
+    honk = ():void => {
+        console.log(`${this.name} is saying :Toooooooooooot!`)
+    }
+
+    accelerate(speed:number):void{
+        this.acceleration = this.acceleration + speed;
+    }
+}
+let car = new Car("BMW");
+car.honk();
+console.log(car.acceleration);
+car.accelerate(60);
+console.log(car.acceleration);
---
+++ lab3/src/exe4.ts
@@ -0,0 +1,41 @@
+/*Exercise 04
+Re-write the following code using TypeScript Class syntax. Try to be as explicit as possible and 
+add Types to everything you can. When you are done, transpile the TS code to JS code and inspect the JS code.
+var baseObject = { 
+	width: 0, 
+	length: 0 
+}; 
+var rectangle = Object.create(baseObject); 
+
+rectangle.width = 5; 
+rectangle.length = 2; 
+
+rectangle.calcSize = function() { 
+	return this.width * this.length; 
+}; 
+
+console.log(rectangle.calcSize()); // 10
+*/
+
+//solution
+
+abstract class Rectangle {
+    calcSize = ():number =>{
+        return 0;
+    };
+}
+
+ class BaseObject extends Rectangle{
+    width:number =0
+    length :number =0
+    }
+
+
+let rectangle :BaseObject = new BaseObject();
+rectangle.width =5;
+rectangle.length =2;
+rectangle.calcSize = function(){
+    return this.length * this.width;
+}
+
+console.log(rectangle.calcSize());
