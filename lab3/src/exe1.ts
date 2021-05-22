12d0539ca7fd95fb81fb0e958c3153609b3766ef
JRKhatri <79480451+JRKhatri@users.noreply.github.com>
2021-05-21 21:30:06 -0500

    lab 3 exercise 1

---
+++ lab3/src/exe1.ts
@@ -0,0 +1,52 @@
+/*Exercise 01
+Re-write the following code using TypeScript. Try to be as explicit as possible and add Types to everything you can.
+ When you are done, transpile the TS code to JS code and inspect the JS code.*/
+
+ /* function University(name, dept) { 
+	this.name = name; 
+	this.dept = dept; 
+	this.graduation = function(year) { 
+		 console.log(`Graduating ${this.dept} ${year} students`); 
+	} 
+} 
+var miu = new Univeristy("MIU", "Computer Science"); 
+miu.graduation(2019); */
+
+//solutions
+//one way
+
+class University {
+    name : string;
+    dept : string;
+    graduation = (year : number) =>{
+        console.log(`Graduating ${this.dept} ${year} students`)
+    }
+
+    constructor(name : string, dept : string){
+        this.name = name;
+        this.dept = dept; 
+    } 
+}
+let miu = new University("MIU", "Computer Science");
+miu.graduation(2019);
+
+// second way(not good some bugs)
+// function University(this: any,  name:string, dept :string):void {
+//     this.name = name;
+//     this.dept = dept;
+//     this.graduation = function(year:number){
+//         console.log(`Graduating ${this.dept} ${year} students`); 
+//     }
+// }
+
+ 
+//third way(not good some bugs)
+// function University(this: any,  name:string, dept :string):void {
+//     this.name = name;
+//     this.dept = dept;
+//     this.graduation = (year:number)=>{
+//         console.log(`Graduating ${this.dept} ${year} students`); 
+//     }
+// }
+// var miu =  University("MIU", "Computer Science");
+// miu.graduation(2019)
